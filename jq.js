fa443a9db9a8b8895df24c9ad64f455348bd9fa7
StanleyHuangEE <stanley850904@gmail.com>
2016-10-12 00:26:08 +0800

    a lot

--- jq.js
+++ jq.js
@@ -1,11 +1,6 @@
 
 var state;
 $(".pp").click(function() {
-  $("#ppc").animate({
-      opacity: 0.25,
-          left: "+=50",
-              height: "toggle"
-                }, 5000, function() {
-                    // Animation complete.
-                      });
-                      });
+  $("#ppc").slideDown("slow",function(){
+  });
+ });
