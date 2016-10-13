78aeba1b9700ba8f08bfc34520bdf8f7690b4fd5
StanleyHuangEE <stanley850904@gmail.com>
2016-10-14 02:34:06 +0800

    add contact & footer

--- jq.js
+++ jq.js
@@ -1,8 +1,22 @@
- $(document).ready(function(){
-     $("button").click(function(){
-             $("aa").slideUp();
-                 });
-                     $("button").click(function(){
-                             $("aa").slideDown();
-                                 });
-                                 });
+var stateP=0;
+
+$(document).ready(function(){
+    $(".pp").slideUp();
+        $(".personalProfile").mouseover(function(){
+              $(".pp").slideDown();
+                                });
+                                        $(".personalProfile").mouseout(function(){
+                                                    $(".pp").slideUp();
+                                                            });
+                                                            });
+
+$(document).ready(function(){
+    $(".cc").slideUp();
+        $(".contact").mouseover(function(){
+            $(".cc").slideDown();
+        });
+
+    $(".contact").mouseout(function(){
+        $(".cc").slideUp();
+    });
+})
